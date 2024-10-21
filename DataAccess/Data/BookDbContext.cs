e4cec9bbe3d39c5d506fd58a9f60888a890a9dcc
dianam77 <diana.mirrezaee@gmail.com>
2024-10-21 22:11:05 +0330

    Add project files.

---
+++ DataAccess/Data/BookDbContext.cs
@@ -0,0 +1,40 @@
+﻿using DataAccess.Models;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Data
+{
+    public class BookDbContext : IdentityDbContext<User, Role,string>
+    {
+        public BookDbContext(DbContextOptions<BookDbContext> options) : base(options)
+        {
+
+        }
+
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<BasketItems> BasketItems { get; set; }
+
+    }
+
+
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<BookDbContext>
+    {
+        public BookDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<BookDbContext>();
+            optionsBuilder.UseSqlServer("Server =.\\SQL1; Database = BookShop; User Id = sa; Password = diana1377; Trusted_Connection = True; TrustServerCertificate = True;");
+
+            return new BookDbContext(optionsBuilder.Options);
+        }
+    }
+}
